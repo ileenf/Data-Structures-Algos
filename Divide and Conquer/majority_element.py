6c5052aaae5851d1a55a4685d239c9158ac77e4b
ileenf <52259696+ileenf@users.noreply.github.com>
2022-01-30 19:27:00 -0800

    Add files via upload

---
+++ Divide and Conquer/majority_element.py
@@ -0,0 +1,19 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        def divide(nums, begin, end):
+            if end==begin:
+                return nums[end]
+            mid = (begin+end)//2
+            
+            left = divide(nums, begin, mid)
+            right = divide(nums, mid+1, end)
+            
+            if left == right:
+                return left
+            if nums[begin:mid+1].count(left) > nums[mid+1:end+1].count(right):
+                return left
+            else:
+                return right
+            
+        return divide(nums, 0, len(nums)-1)
+            
