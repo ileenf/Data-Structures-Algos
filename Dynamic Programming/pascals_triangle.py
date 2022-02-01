8d5864606e081797c87a7a3feba02452329c1b3b
ileenf <52259696+ileenf@users.noreply.github.com>
2022-02-01 12:59:06 -0800

    Create pascals_triangle.py

---
+++ Dynamic Programming/pascals_triangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        rows = [[1]]
+        
+        for i in range(0, numRows-1):
+            prev_row = rows[i] 
+            new_row = [1 for _ in range(len(prev_row)+1)]
+            
+            for j in range(len(new_row)):
+                if j != 0 and j != len(new_row)-1:
+                    new_row[j] = prev_row[j-1] + prev_row[j]
+                    
+            rows.append(new_row)
+            
+        return rows
