b2c85a2c1ebbe527a8e3b7232d838aec4e22ef86
ileenf <52259696+ileenf@users.noreply.github.com>
2022-02-01 13:44:16 -0800

    Update fibonacci.py

--- Dynamic Programming/fibonacci.py
+++ Dynamic Programming/fibonacci.py
@@ -1,7 +1,8 @@
 class Solution:
+    # dp
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
+        if n <= 1:
+            return n
         dp = [0 for _ in range(n+1)]
         dp[1] = 1
         
@@ -9,3 +10,16 @@ class Solution:
             dp[i] = dp[i-1] + dp[i-2]
             
         return dp[n]
+    
+    # recursion with memoization
+    def fib(self, n: int) -> int:
+        memo = {0:0, 1:1}
+        
+        def recur(n):
+            if n in memo:
+                return memo[n]
+
+            memo[n] = recur(n-1) + recur(n-2)
+            return memo[n]
+            
+        return recur(n)
