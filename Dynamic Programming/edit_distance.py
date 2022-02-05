b2bad037c9c93aa53dcab5c57cddd5d691e032cc
ileenf <52259696+ileenf@users.noreply.github.com>
2022-02-05 00:20:48 -0800

    Create edit_distance.py

---
+++ Dynamic Programming/edit_distance.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n = len(word1)
+        m = len(word2)
+        
+        dp = [[0] * (m+1) for _ in range(n+1)]
+        
+        for i in range(n+1):
+            dp[i][0] = i
+        for j in range(m+1):
+            dp[0][j] = j
+        
+        for i in range(1, len(word1)+1):
+            for j in range(1, len(word2)+1):
+                insert = dp[i][j-1] + 1
+                delete = dp[i-1][j] + 1
+                replace = dp[i-1][j-1] + (word1[i-1] != word2[j-1])
+                dp[i][j] = min(insert, delete, replace)
+            
+        return dp[-1][-1]
