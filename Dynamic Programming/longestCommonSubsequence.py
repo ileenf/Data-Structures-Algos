af3fe3254a5082730cbda66db51bc38cf562007a
ileenf <52259696+ileenf@users.noreply.github.com>
2022-06-16 16:15:59 -0700

    Update longestCommonSubsequence.py

--- Dynamic Programming/longestCommonSubsequence.py
+++ Dynamic Programming/longestCommonSubsequence.py
@@ -1,4 +1,25 @@
 class Solution:
+    # recursion + memoization
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        memo = dict()
+        def helper(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if i < 0 or j < 0:
+                return 0
+            if text1[i] == text2[j]:
+                
+                memo[(i, j)] =  1 + helper(i-1, j-1)
+            else:
+                case2 = helper(i-1, j)
+                case3 = helper(i, j-1)
+                
+                memo[(i, j)] = max(case2, case3)
+            return memo[(i, j)]
+        
+        return helper(len(text1)-1, len(text2)-1)
+    
+    # dp
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         dp = [[0] * (len(text1)+1) for _ in range(len(text2)+1)] 
         
