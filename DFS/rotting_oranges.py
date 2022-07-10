2fb9f69ececf2e580e7147e7964f40b384853984
ileenf <52259696+ileenf@users.noreply.github.com>
2022-07-10 12:58:57 -0700

    Create rotting_oranges.py

---
+++ DFS/rotting_oranges.py
@@ -0,0 +1,37 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        def inbounds(row, col, rows, cols):
+            return row >= 0 and row < rows and col >= 0 and col < cols
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        minutes = 0
+        fresh = 0
+        rotten_oranges = deque()
+        
+        for row in range(rows):
+            for col in range(cols):
+                orange = grid[row][col] 
+                if orange == 1:
+                    fresh += 1
+                elif orange == 2:
+                    rotten_oranges.append([row, col])
+        
+        while rotten_oranges:
+            for i in range(len(rotten_oranges)):
+                row, col = rotten_oranges.popleft()
+                
+                for dr, dc in [[0,1], [0,-1], [1,0], [-1,0]]:
+                    r = dr + row
+                    c = dc + col
+                    
+                    if inbounds(r, c, rows, cols) and grid[r][c] == 1:
+                        # fruit has turned rotten from fresh
+                        fresh -= 1
+                        grid[r][c] = 2
+                        rotten_oranges.append([r, c])
+            minutes += 1
+        if fresh != 0:
+            return -1
+        return max(0, minutes-1)
