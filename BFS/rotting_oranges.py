33c547e50ab99e9799c313c57a75be7a029cf7e0
ileenf <52259696+ileenf@users.noreply.github.com>
2022-07-10 13:05:48 -0700

    Update rotting_oranges.py

--- BFS/rotting_oranges.py
+++ BFS/rotting_oranges.py
@@ -1,5 +1,11 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        # N is number of elements in grid
+        # time complexity - O(N) since we traverse through each element once in the
+        
+        # nested for loop, and at most each element once through the queue
+        # space complexity - O(N) since the queue will be store at most
+        # the amount of elements in grid
         def inbounds(row, col, rows, cols):
             return row >= 0 and row < rows and col >= 0 and col < cols
         
@@ -35,3 +41,4 @@ class Solution:
         if fresh != 0:
             return -1
         return max(0, minutes-1)
+    
