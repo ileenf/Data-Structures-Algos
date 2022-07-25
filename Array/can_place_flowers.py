adb9d535b58b11c38b087498a0f4f25b81d8e821
ileenf <52259696+ileenf@users.noreply.github.com>
2022-07-24 23:38:47 -0700

    Update can_place_flowers.py

--- Array/can_place_flowers.py
+++ Array/can_place_flowers.py
@@ -3,17 +3,14 @@ class Solution:
     # time complexity: O(N), scan through flowerbed once
     # space complexity: O(1), only storing bools/ints, and indexing is constant
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        placed = False
         num_placed = 0
         flowerbed = [0] + flowerbed + [0]
         for i in range(1, len(flowerbed)-1):
             if flowerbed[i] == 1:
                 placed = True
                 continue
-            elif not placed and flowerbed[i+1] == 0 and flowerbed[i-1] == 0:
+            elif flowerbed[i+1] == 0 and flowerbed[i-1] == 0:
                 num_placed += 1
-                placed = True
-            else:
-                placed = False
+                flowerbed[i] = 1
                 
         return num_placed >= n
