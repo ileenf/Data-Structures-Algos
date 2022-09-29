43d41c66e99a4e447523f25e0f4160ff472d2efe
ileenf <52259696+ileenf@users.noreply.github.com>
2022-09-28 23:23:53 -0700

    Create campus_bikes.py

---
+++ Array/campus_bikes.py
@@ -0,0 +1,23 @@
+class Solution:
+    def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> List[int]:
+        def manhattan_dist(x1, y1, x2, y2):
+            return abs(x1-x2) + abs(y1-y2)
+        
+        distances = []
+        
+        for i, worker in enumerate(workers):
+            for j, bike in enumerate(bikes):
+                dist = manhattan_dist(worker[0], worker[1], bike[0], bike[1])
+                distances.append([dist, i, j])
+        
+        sorted_distances = sorted(distances)
+        
+        worker_bike_pos = [-1 for _ in range(len(workers))]
+        assigned_bikes = set()
+        
+        for dist, worker, bike in sorted_distances:
+            if worker_bike_pos[worker] == -1 and bike not in assigned_bikes:
+                worker_bike_pos[worker] = bike
+                assigned_bikes.add(bike)
+
+        return worker_bike_pos
