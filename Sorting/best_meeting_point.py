f7eb2d4f28a6551558340562e8456cc4adcc5efe
ileenf <52259696+ileenf@users.noreply.github.com>
2022-10-24 23:36:42 -0700

    Add files via upload

---
+++ Sorting/best_meeting_point.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minTotalDistance(self, grid: List[List[int]]) -> int:
+        house_x_positions = []
+        house_y_positions = []
+        total_dist = 0
+
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if grid[x][y] == 1:
+                    house_x_positions.append(x)
+                    house_y_positions.append(y)
+
+        house_x_positions.sort()
+        house_y_positions.sort()
+        median_x = house_x_positions[len(house_x_positions)//2]
+        median_y = house_y_positions[len(house_y_positions)//2]
+
+        for i in range(len(house_x_positions)):
+            x = house_x_positions[i]
+            y = house_y_positions[i]
+            dist = abs(median_x - x) + abs(median_y - y)
+            total_dist += dist
+
+        return total_dist
