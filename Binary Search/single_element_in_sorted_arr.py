b8a3836f1d0fa46a15c87915197ec188ab2b080e
ileenf <52259696+ileenf@users.noreply.github.com>
2023-04-10 23:04:20 -0700

    Update single_element_in_sorted_arr.py

--- Binary Search/single_element_in_sorted_arr.py
+++ Binary Search/single_element_in_sorted_arr.py
@@ -10,7 +10,7 @@ class Solution:
             if mid % 2 == 0:
                 # correct, misalignment on right half
                 if mid < len(nums) - 1 and nums[mid+1] == nums[mid]:
-                    left = mid + 1
+                    left = mid + 2
                 else:
                     right = mid - 1
             
@@ -20,5 +20,27 @@ class Solution:
                 if nums[mid-1] == nums[mid]:
                     left = mid + 1
                 else:
-                    right = mid - 1
+                    right = mid - 2
+        return nums[left]
+    
+    
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        # single element at first even idx that doesn't have a duplicate to right
+        # after the single element, all duplicate pairs after will be misaligned
+        # FFFFTTTT pattern
+
+
+        left = 0
+        right = len(nums) - 1
+        
+        # binary search only even indexes
+        while left < right:
+            mid = (left + right) // 2
+            if mid % 2 == 1:
+                mid -= 1
+
+            if nums[mid] != nums[mid+1]:
+                right = mid
+            else:
+                left = mid + 2
         return nums[left]
