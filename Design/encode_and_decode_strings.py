f8433915c9a8a7d8aa38f80d54295bf26ccd9384
ileenf <52259696+ileenf@users.noreply.github.com>
2023-05-21 00:15:08 -0700

    Create encode_and_decode_strings.py

---
+++ Design/encode_and_decode_strings.py
@@ -0,0 +1,11 @@
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        return 'delimeter'.join(strs)
+        
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        return s.split('delimeter')
