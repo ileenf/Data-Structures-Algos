79911930b33be5702df7041a004e498ec456d455
ileenf <52259696+ileenf@users.noreply.github.com>
2023-05-21 12:49:56 -0700

    Create fruit_into_baskets.py

---
+++ Hashtable/fruit_into_baskets.py
@@ -0,0 +1,26 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        i = 0
+        j = 1
+        
+        # keep track of amount of fruit for each type
+        curr_fruits = defaultdict(int)
+        curr_fruits[fruits[i]] += 1
+        max_fruits = 1
+
+        while i < j and j < len(fruits):
+            curr_fruits[fruits[j]] += 1
+
+            if len(curr_fruits) <= 2:
+                max_fruits = max(max_fruits, j-i+1)
+
+            else:
+                curr_fruits[fruits[i]] -= 1
+
+                if curr_fruits[fruits[i]] == 0:
+                    del curr_fruits[fruits[i]]
+                i += 1
+            j += 1
+
+                
+        return max_fruits
