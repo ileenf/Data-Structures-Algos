021dd3dd9d347c0ca35775e9251fcb9e2f138d5f
ileen fan <52259696+ileenf@users.noreply.github.com>
2025-09-18 22:37:22 -0700

    Update best_time_to_buy_and_sell_stock.py

--- Array/best_time_to_buy_and_sell_stock.py
+++ Array/best_time_to_buy_and_sell_stock.py
@@ -11,3 +11,13 @@ class Solution:
             max_profit = max(max_profit, price - min_price)
         return max_profit
             
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        buy_price = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < buy_price:
+                buy_price = prices[i]
+            max_profit = max(max_profit, prices[i] - buy_price)
+
+        return max_profit
