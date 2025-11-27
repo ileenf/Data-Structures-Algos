d7cb3aa018c598039ca2e3e0d682eaa8b7e13c17
ileen fan <52259696+ileenf@users.noreply.github.com>
2025-11-26 20:56:37 -0800

    Update reverse_words.py

--- String/reverse_words.py
+++ String/reverse_words.py
@@ -1,5 +1,4 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        words = s.split(' ')
-        reversed_words = [word for word in words[::-1] if word]
-        return ' '.join(reversed_words)
+        words = s.split()
+        return ' '.join(words[::-1])
